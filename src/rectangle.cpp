a76d07ef628bec21d2c3127f3dcd976d44bbd624
Cindy <cindy.do10@gmail.com>
2020-11-09 20:12:51 -0800

    defined rectangle class

---
+++ src/rectangle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "../header/rectangle.hpp"
+
+using namespace std;
+
+Rectangle::Rectangle() {}
+
+Rectangle::Rectangle(int w, int h) {
+    width = w;
+    height = h;
+}
+
+void Rectangle::set_width(int w) {
+    width = w;
+}
+
+void Rectangle::set_height(int h) {
+    height = h;
+}
+
+int Rectangle::area() {
+    return width *  height;
+}
+
+int Rectangle::perimeter() {
+    int perimeter = 2 * (width + height);
+    return perimeter;
+}
